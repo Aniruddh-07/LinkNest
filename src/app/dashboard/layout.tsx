9430b169fcf842b37c48e4a9f7ed1328915bf957
Aniruddh Upadhyay <aniruddhupadhyay07@gmail.com>
2025-09-01 08:51:41 +0000

    it still working do one thing remove this hamburger button and rewrite i

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -24,11 +24,8 @@ function DashboardHeader() {
             <span className="sr-only">Toggle Menu</span>
           </Button>
         </SheetTrigger>
-        <SheetContent side="left" className="p-0 sm:max-w-xs">
-          <SheetHeader className="sr-only">
-             <SheetTitle>Sidebar Menu</SheetTitle>
-          </SheetHeader>
-          <Sidebar />
+        <SheetContent side="left" className="p-0">
+           <Sidebar />
         </SheetContent>
       </Sheet>
     </header>
